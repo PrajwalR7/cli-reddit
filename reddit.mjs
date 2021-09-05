06e8d08b829afd65d20c8cecd0e366ae2ffa92fd
Prajwal R <rameshprajwal21@gmail.com>
2021-09-05 12:58:53 +0530

    added new functionalities

--- reddit.mjs
+++ reddit.mjs
@@ -31,34 +31,34 @@ program
     })
 
 program
+    .command('new')
+    .alias('n')
+    .description('Generate a random new reddit post')
+    .action(()=>{
+        newpost();
+    })
+
+program
+    .command('rising')
+    .alias('r')
+    .description('Generate a random rising reddit post')
+    .action(()=>{
+        rising();
+    })
+
+program 
+    .command(`search <string>`)
+    .alias('s')
+    .description('Search on reddit')
+    .action(({string})=>{
+        console.log(string);
+        search({string});
+    })
+
+program
     .option('--print, --p','print the post link and title in console')
 
 program.parse(process.argv);
-// yargs.usage('Usage: $0 <command> [OPTIONS]')
-// yargs.help('h')
-// yargs.alias('h','help').argv
-// const argv =  yargs
-// .command({
-//     "command":'homepost',
-//     "describe":"fetching homepost",
-//     handler:
-//     (argv)=>{
-//         homepost();
-//     }
-// }).argv
-
-// if(argv.top || argv.t){
-//     top();
-// }
-// else if(argv.homepost || argv.hp){
-//     homepost();
-// }
-// else if(argv.help || argv.h){
-//     printHelp("",true)
-// }
-// else{
-//     printHelp("Incorrect Usage",true)
-// }
 
 async function top(){
     try {
@@ -97,25 +97,55 @@ async function homepost(){
         console.log(e);
     }
 }
-    
-// function printHelp(msg,includeHelp=false){
-//     console.error(chalk.red(msg));
-//     if(includeHelp){
-//         console.log("");
-//         console.log(chalk.cyan("reddit usage:"));
-//         console.log("");
-//         console.log(chalk.green("   reddit --[COMMAND] --[OPTIONS]"));
-//         console.log("");
-//         console.log(chalk.grey("   reddit --help or --h"));
-//         console.log("");
-//         console.log(chalk.gray("   reddit -homepost or --hp"));
-//         console.log("");
-//         console.log(chalk.gray("   reddit --top or --t"));
-//         console.log("");
-//         console.log(chalk.blue("   OPTIONS include --print"));
-//     }
-// }
 
+async function newpost(){
+    try {
+        const res = await fetch("https://www.reddit.com/new/.json");
+        const data = await res.json();
+        const children = data.data.children;
+        const randompost = children[Math.floor(Math.random()*children.length)];
+        const link = `https://reddit.com${randompost.data.permalink}`;
+        if(options.print){
+            onlyprint(randompost.data.title,link)
+        }
+        else{
+            open(link);
+        }
+    } catch (error) {
+        var e = chalk.red("Couldnt fetch the requested url")
+        console.log(e);
+    }
+}
+
+async function rising(){
+    try {
+        const res = await fetch("https://www.reddit.com/rising/.json");
+        const data = await res.json();
+        const children = data.data.children;
+        const randompost = children[Math.floor(Math.random()*children.length)];
+        const link = `https://reddit.com${randompost.data.permalink}`;
+        if(options.print){
+            onlyprint(randompost.data.title,link)
+        }
+        else{
+            open(link);
+        }
+    } catch (error) {
+        var e = chalk.red("Couldnt fetch the requested url")
+        console.log(e);
+    }
+}
+
+async function search(string){
+    try {
+        //const searchelement = await string;
+        console.log(string);
+       await open(`https://reddit.com/search/?q=${string}`);
+    } catch (error) {
+        var e = chalk.red("Couldnt fetch the requested url")
+        console.log(e);
+    }
+}
 
 function onlyprint(title ,link){
     let ctitle = chalk.yellow(title)
